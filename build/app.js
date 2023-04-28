eaa9149b2b3b70c65527ac1fb86d59ff3202519c
Rocky Jay <rockyjay_g@yahoo.com>
2023-04-28 03:18:03 -0700

    adding new img and content of slideshow

--- build/app.js
+++ build/app.js
@@ -292,6 +292,166 @@ async function getData() {
     }, 2500);
   }
 
-  // startInterVal();
+  startInterVal();
 }
 getData();
+
+const slideShowData = [
+  {
+    mobileImg: "../img/slideShow-mobile-1.png",
+    desktopImg: "../img/slideShow-desktop-1.png",
+    headingImg: "../img/slideShow-header-1.png",
+    header: " EGS Exclusive - Out Now",
+    paragraph:
+      "Uncover the truth behind the outbreak and discover who or what you are",
+    price: "Php 2,199,00",
+    buttonText: "buy now",
+  },
+  {
+    mobileImg: "../img/slideShow-mobile-2.png",
+    desktopImg: "../img/slideShow-desktop-2.png",
+    headingImg: "../img/slideShow-header-2.png",
+    header: "Now Availabe",
+    paragraph:
+      "HoYoverse's Honkai: Star Rail is available now! Climb aboard to the stars!",
+    price: "Free",
+    buttonText: "Play for free",
+  },
+  {
+    mobileImg: "../img/slideShow-mobile-3.png",
+    desktopImg: "../img/slideShow-desktop-3.png",
+    headingImg: "../img/slideShow-header-3.png",
+    header: "Now Availabe",
+    paragraph:
+      "Join Jedi Knight Cal Kestis in STAR WARS Jedi: Survivor™, a galaxy-spanning, action-packed adventure.",
+    price: "Php 2,999.00",
+    buttonText: "out now",
+  },
+  {
+    mobileImg: "../img/slideShow-mobile-4.png",
+    desktopImg: "../img/slideShow-desktop-4.png",
+    headingImg: "../img/slideShow-header-4.png",
+    header: "Comming May 2",
+    paragraph:
+      "Uncover the Truth, Take Back Redfall in this new open-world, co-op FPS from Arkane Austin.",
+    price: "Php 2,099.00",
+    buttonText: "pre-purchase now",
+  },
+  {
+    mobileImg: "../img/slideShow-mobile-5.png",
+    desktopImg: "../img/slideShow-desktop-5.png",
+    headingImg: "../img/slideShow-header-5.png",
+    header: "Last Chance to Pre-Purchase",
+    paragraph:
+      "Explore new magical realms in Age of Wonders’ signature blend of 4X strategy and turn-based tactical combat.",
+    price: "Php 1,859.00",
+    buttonText: "pre-purchase now",
+  },
+  {
+    mobileImg: "../img/slideShow-mobile-6.png",
+    desktopImg: "../img/slideShow-desktop-6.png",
+    headingImg: "../img/slideShow-header-6.png",
+    header: "EGS Exclusive",
+    paragraph:
+      "Take on the role of Travis Baker – a man with his sights set on becoming the new King of Rockay City, one crime at a time…",
+    price: "Php 1,077.99",
+    buttonText: "Now availabe",
+  },
+];
+
+const mainSlideShow = document.querySelector("#mainSlideShow");
+
+slideShowData.forEach((element, index) => {
+  const {
+    mobileImg,
+    desktopImg,
+    headingImg,
+    header,
+    paragraph,
+    price,
+    buttonText,
+  } = element;
+
+  const value =
+    price === "Free"
+      ? `<p>${price}</p>`
+      : `<p>Starting at</p>
+  <p>${price}</p>`;
+
+  const html = `<div
+  class="grow-0 shrink-0 h-full w-96 snap-center rounded-2xl md:w-[85%] relative overflow-hidden lg:w-[100%]"
+  id="page-${index + 1}"
+>
+  <picture>
+    <source
+      media="(max-width: 768px)"
+      srcset="${mobileImg}"
+    />
+    <img
+      src="${desktopImg}"
+      alt="slide show logo "
+      class="rounded-md block absolute h-full w-full object-cover top-0 left-0"
+    />
+  </picture>
+
+  <div
+    class="relative h-20 w-14 ml-auto right-0 top-0 flex justify-center items-center z-40 2xl:hidden"
+  >
+    <div
+      class="h-7 w-7 rounded-full bg-black border-2 border-white flex justify-center items-center text-white"
+    >
+      <span class="material-symbols-outlined text-white text-2xl">
+        add
+      </span>
+    </div>
+  </div>
+
+  <article
+    class="w-full absolute flex flex-col gap-4 bottom-0 text-white pl-4 pb-8 z-10 md:pl-10 md:pr-52 2xl:w-[50%] 2xl:pr-0 2xl:pb-16 2xl:pl-16"
+  >
+    <img
+      src="${headingImg}"
+      alt=""
+      class="w-40 md:w-52 2xl:w-72 2xl:mb-8"
+    />
+    <p class="text-[0.6rem] font-bold md:text-sm">
+      ${header}
+    </p>
+    <p
+      class="font-bold leading-5 md:text-lg md:leading-6 2xl:leading-7 2xl:max-w-xs"
+    >
+     ${paragraph}
+    </p>
+    <div
+      class="flex flex-col font-semibold leading-3 2xl:flex-row gap-2 2xl:font-medium"
+    >
+      ${value}
+    </div>
+
+    <div class="hidden 2xl:flex gap-2 mt-6">
+      <button
+        class="block font-medium bg-white text-black rounded-sm py-3 px-${
+          buttonText === "buy now" ? "5" : "7"
+        } uppercase text-sm -mt-2"
+      >
+        ${buttonText}
+      </button>
+      <button
+        class="flex items-center gap-2 font-medium hover:bg-white/20 text-white rounded-sm py-3 px-7 uppercase text-xs -mt-2 transition-all duration-200"
+      >
+        <div
+          class="h-5 w-5 rounded-full border-2 border-white flex justify-center items-center"
+        >
+          <span class="material-symbols-outlined text-sm font-bold">
+            add
+          </span>
+        </div>
+        add to wishlist
+      </button>
+    </div>
+  </article>
+  <div class="absolute h-full w-full top-0 left-0 bg-img"></div>
+</div>`;
+
+  mainSlideShow.insertAdjacentHTML("beforeend", html);
+});
